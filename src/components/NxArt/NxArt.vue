77348d01876dba51cb71955914603b335e102377
wang.haoyu <228233464@qq.com>
2021-01-27 18:02:33 +0800

    fix: phone art margin

--- src/components/NxArt/NxArt.vue
+++ src/components/NxArt/NxArt.vue
@@ -67,37 +67,25 @@ export default {
   }
 }
 
-/* pad格式 暂时不处理 业务没有pad 当作手机横屏处理 */
+/* pad格式 暂时不处理 业务没有pad 当作pc处理 */
 @media screen and (min-width: 600px) and (max-width: 960px) {
   .nx-art {
     width: 80%;
     margin: 0 auto;
     padding: 0.5rem 0;
-    //	@for $i from 1 through 3 {
-    //		h#{$i} {
-    //			font-size: 3-$i*.6rem;
-    //			line-height:3-$i*.5rem;
-    //		}
-    //}
-    h1 {
-      font-size: 1.6rem;
-      line-height: 2rem;
-    }
-    h2 {
-      font-size: 1.4rem;
-      line-height: 1.8rem;
-    }
-    h3 {
-      font-size: 1.2rem;
-      line-height: 1.6rem;
+    @for $i from 1 through 3 {
+      h#{$i} {
+        font-size: 1-$i * 0.2rem;
+        line-height: 1rem;
+      }
     }
     p,
     li {
-      font-size: 1rem;
-      line-height: 1.2rem;
+      font-size: 0.3rem;
+      line-height: 0.5rem;
     }
     p {
-      //text-indent: 1rem;
+      //text-indent: 0.6rem;
     }
     li > p {
       text-indent: 0;
@@ -158,14 +146,17 @@ export default {
     h1 {
       font-size: 1.6rem;
       line-height: 2rem;
+      margin: 5px 0;
     }
     h2 {
       font-size: 1.4rem;
       line-height: 1.8rem;
+      margin: 5px 0;
     }
     h3 {
       font-size: 1.2rem;
       line-height: 1.6rem;
+      margin: 5px 0;
     }
     p,
     li {
